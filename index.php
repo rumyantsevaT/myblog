bd895df46961ab8d10dd5994a9bd5f7a184c30ff
ipochinili <ipochinili@gmail.com>
2018-12-25 01:42:18 +0300

    add variable _post

--- index.php
+++ index.php
@@ -22,14 +22,14 @@ var_dump($result);
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <div class="container">
-        <a class="navbar-brand" href="/localhost/myblog.loc">Rumyantseva-blog</a>
+        <a class="navbar-brand" href="/myblog.loc">Rumyantseva-blog</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarText">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Новости<span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="/myblog.loc">Новости<span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="new-post.php">Написать статью</a>
@@ -51,8 +51,8 @@ var_dump($result);
     <div class="row">
         <div class="col-12 col-md-4">
             <div class="jumbotron">
-                <h1 class="display-4"><?= $_POST["title"];?></h1>
-                <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                <h1 class="display-4"><?=$_POST["title"];?></h1>
+                <p class="lead"><?=$_POST["text"];?></p>
                 <hr class="my-4">
                 <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
                 <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
--- new-post.php
+++ new-post.php
@@ -12,26 +12,26 @@
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-<div class="container">
-	<a class="navbar-brand" href="#">Rumyantseva-blog</a>
-	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-		<span class="navbar-toggler-icon"></span>
-	</button>
-	<div class="collapse navbar-collapse" id="navbarText">
-		<ul class="navbar-nav mr-auto">
-			<li class="nav-item active">
-				<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">Features</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">Pricing</a>
-			</li>
-		</ul>
-		<span class="navbar-text"><button type="button" class="btn btn-outline-light">Регистрация</button></span>
-	</div>
-</div><!-- /.container -->
+    <div class="container">
+        <a class="navbar-brand" href="/myblog.loc">Rumyantseva-blog</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="/myblog.loc">Новости</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="new-post.php">Написать статью<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Отредактировать статью</a>
+                </li>
+            </ul>
+            <span class="navbar-text"><button type="button" class="btn btn-outline-light">Регистрация</button></span>
+        </div>
+    </div><!-- /.container -->
 </nav>
 <div class="container">
 	<div class="row">
